0f90f50bff6c50ac6e8b56b53fd56804fb4e9a0b
sandeepsannap <sandeep50sannap@gmail.com>
2021-06-01 08:00:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./sidebar";
+import Topbar from "./topbar";
+import Dashboard from "./dashboard/dashboard";
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar></Topbar>
+            <div class="container-fluid">
+              <Dashboard></Dashboard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
